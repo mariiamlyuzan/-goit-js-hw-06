857e187c253d71ee8a38e8cc81e5dd1e6736ec19
mariiamlyuzan <86401047+mariiamlyuzan@users.noreply.github.com>
2021-10-25 12:36:44 +0300

    add changes

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -3,15 +3,13 @@ const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 const initialValue = document.querySelector('#value');
 
-let counterValue = 1;
+let counterValue = 0;
 decrementBtn.addEventListener ('click', () => {
-    initialValue.textContent = counterValue;
     counterValue -= 1;
-
+    initialValue.textContent = counterValue;
 });
 
 incrementBtn.addEventListener('click', () => {
-    initialValue.textContent = counterValue;
     counterValue += 1;
-     
+    initialValue.textContent = counterValue;
 });
--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,5 +3,12 @@ const inputRef = document.querySelector('#validation-input');
 inputRef.addEventListener('blur', onEnterInput);
 
 function onEnterInput () {
-    inputRef.value.length == inputRef.dataset.length ? inputRef.classList.add('valid') : inputRef.classList.add('invalid');
+   if(inputRef.value.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.remove('invalid');
+    inputRef.classList.add('valid');
+      
+    } else {
+        inputRef.classList.remove('valid');
+        inputRef.classList.add('invalid');
     }
+}
